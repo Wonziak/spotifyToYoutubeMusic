cd6fda11ceacf13680d8334e6adcac0278c71377
Witold Dankiewicz <witold.dankiewicz@gmail.com>
2024-08-11 13:10:44 +0200

    add youtube api

--- README.md
+++ README.md
@@ -1,5 +1,7 @@
 ## Spotify playlist to Youtube Music playlist converter
 
+### Getting started
+
 Run `pip install -r requirements.txt`
 
 Follow steps from: [Authenticating with the Spotipy API](https://www.youtube.com/watch?v=kaBVN8uP358) to create Spotify app.
@@ -7,3 +9,19 @@ Follow steps from: [Authenticating with the Spotipy API](https://www.youtube.com
 Then set your credentials and your spotify playlist id in `settings.py`
 
 To get playlist id, you can copy its link from `Share` in UI, and copy `<playlist_id>` part `/playlist/<playlist_id>`
+
+Before you run this program create playlist in Youtube Music and get its ID like you did for Spotify playlist -
+`playlist?list=<playlist_id` and set it in `settings.py`
+
+To make Youtube Music work, you need to run `ytmusicapi oauth`  and follow instructions from terminal -
+Youtube music does not have any API. It will generate `oauth.json` file needed for it to work.
+
+### Error handling
+Program will generate file `missing_songs.txt`. That contains titles of songs that have different duration than Spotify version -
+by default it is +/- 10 seconds, but can be configurable by `duration_difference` parameter in `settings.py`.  
+Also, this file containes titles of songs that for some reason could not be found in Youtube Music and should be added manually.
+
+### It looks like Youtube API sometimes crashes so this program may require restarts, especially if your playlist is long. Regenerating token with `ytmusicapi oauth` should help.
+
+## Results.
+For my playlist, that contains 1120 songs, this program missed 100.
--- main.py
+++ main.py
@@ -1,23 +1,73 @@
+import json
 import spotipy
 from spotipy.oauth2 import SpotifyClientCredentials
-import json
+from ytmusicapi import YTMusic
 
-from settings import SPOTIPY_CLIENT_ID, SPOTIPY_CLIENT_SECRET, SPOTIFY_PLAYLIST_ID
-
-spotify = spotipy.Spotify(
-    client_credentials_manager=SpotifyClientCredentials(client_id=SPOTIPY_CLIENT_ID,
-                                                        client_secret=SPOTIPY_CLIENT_SECRET))
-
-results = spotify.playlist_items(playlist_id=SPOTIFY_PLAYLIST_ID)
+from settings import SKIP_SPOTIFY, SPOTIPY_CLIENT_ID, SPOTIPY_CLIENT_SECRET, SPOTIFY_PLAYLIST_ID, YOUTUBE_PLAYLIST_ID, \
+    duration_difference
 
 
-tracks = {}
-for offset in range(0, 1301, 100):
-    results = spotify.playlist_items(playlist_id="2RWy0Ml1btnAF1unHJN0S8", offset=offset)
-    for track in results["items"]:
-        tracks[track["track"]["name"]] = {"duration_ms": track["track"]["duration_ms"],
-                                          "artists": [artist["name"] for artist in track["track"]["artists"]]}
+def create_json_from_spotify_playslist(spotify_playlist_id: str = SPOTIFY_PLAYLIST_ID) -> None:
+    """
+    This function reads songs from Spotify playlist and saves song titles, artists and duration in miliseconds in file:
+    songs_from_spotify_playlist.json.
 
-print(tracks)
-print(len(tracks))
-json.dump(tracks, open("songs_from_spotify_playlist.json", "w"))
+    :param spotify_playlist_id: Spotify playlist it to reads songs from
+    :return: None
+    """
+    spotify = spotipy.Spotify(
+        client_credentials_manager=SpotifyClientCredentials(client_id=SPOTIPY_CLIENT_ID,
+                                                            client_secret=SPOTIPY_CLIENT_SECRET))
+
+    tracks = {}
+    for offset in range(0, 1301, 100):
+        results = spotify.playlist_items(playlist_id=spotify_playlist_id, offset=offset)
+        for track in results["items"]:
+            tracks[track["track"]["name"]] = {"duration_ms": track["track"]["duration_ms"],
+                                              "artists": [artist["name"] for artist in track["track"]["artists"]]}
+
+    json.dump(tracks, open("songs_from_spotify_playlist.json", "w"))
+    print("JSON file with Spotify songs from playlist ")
+
+
+def search_for_songs_in_ytmusic_and_add_to_playlist(youtube_playlist_id: str = YOUTUBE_PLAYLIST_ID,
+                                                    duration_difference: int = duration_difference) -> None:
+    """
+    This function searches for song in Youtube music, compares duration to Spotify duration and adds song to playlist.
+    :param youtube_playlist_id: Youtube Music playlist id
+    :param duration_difference: difference of duration in Youtube compared to Spotify
+    :return: None
+    """
+    json_file = open("songs_from_spotify_playlist.json")
+    tracks = json.load(json_file)
+
+    yt = YTMusic('oauth.json')
+
+    for title, data in tracks.items():
+        artists = " ".join(data['artists'])
+        search_results = yt.search(f"{title} {artists}")
+        try:
+            duration_seconds_youtube = search_results[0]["duration_seconds"]
+            duration_seconds_spotify = data["duration_ms"] / 1000
+            if duration_seconds_spotify - duration_difference <= duration_seconds_youtube <= duration_seconds_spotify + duration_difference:
+                yt.add_playlist_items(youtube_playlist_id, [search_results[0]['videoId']])
+                print(search_results[0]["title"])
+            else:
+                print(f"Song '{title}' with similar duration to Spotify not found")
+                with open("missing_songs.txt", "a") as file:
+                    file.write(title + "\n")
+        except KeyError:
+            print(f"Song '{title}' not found in youtube music")
+            with open("missing_songs.txt", "a") as file:
+                file.write(title + "\n")
+    print("All songs checked and added")
+
+
+if __name__ == "__main__":
+    """
+    You can skip creating JSON from Spotify playlist if it was already created and you dont want to recreate it.
+    """
+    if not SKIP_SPOTIFY:
+        create_json_from_spotify_playslist()
+    search_for_songs_in_ytmusic_and_add_to_playlist(youtube_playlist_id=YOUTUBE_PLAYLIST_ID,
+                                                    duration_difference=duration_difference)
